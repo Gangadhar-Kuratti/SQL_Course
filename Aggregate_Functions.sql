bae2ccd53b612a7eda5f4cc2f8a94f5b3d95237a
Gangadhar <gangadharkuratti790@gmail.com>
2026-07-23 22:02:57 +0530

    added program

---
+++ Aggregate_Functions.sql
@@ -0,0 +1,13 @@
+-- Snippet to understand the aggregate functions
+
+SELECT min(marks)  -- min function is to find the minimum of group
+FROM students;
+
+SELECT max(marks)  -- min function is to find the maximum of group
+FROM students;
+
+SELECT avg(marks)  -- min function is to find the average of group
+FROM students;
+
+SELECT sum(marks)  -- sum function is to find the total of group
+FROM students;
